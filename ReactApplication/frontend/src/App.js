3434957e5974c5426e8f601264bee7b5100ab810
arty4325 <arturo.acuna.duran@gmail.com>
2022-11-22 20:16:08 -0600

    Continuacion del frontend

--- ReactApplication/frontend/src/App.js
+++ ReactApplication/frontend/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Appbar from './components/Appbar';
+
 
 function App() {
   return (
     <div className="App">
-      Prueba del frontend
+    <Appbar/>
     </div>
   );
 }
