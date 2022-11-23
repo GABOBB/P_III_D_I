2292883a7ba60dcaba810a5b195e125f3ede2a1c
arty4325 <arturo.acuna.duran@gmail.com>
2022-11-23 01:52:10 -0600

    Se cambio la implementacion de la base de datos para que se acople al grafo

--- pruebaMongo/src/main/java/com/example/pruebaMongo/model/Avenger.java
+++ pruebaMongo/src/main/java/com/example/pruebaMongo/model/Avenger.java
@@ -6,20 +6,15 @@ public class Avenger {
     @Id
     private Long id;
 
-    private String name;
-    private String superheroName;
-    private String movie;
-    private Integer age;
+    private String information;
+
 
     public Avenger() {
     }
 
-    public Avenger(Long id, String name, String superheroName, String movie, Integer age) {
+    public Avenger(Long id, String information) {
         this.id = id;
-        this.name = name;
-        this.superheroName = superheroName;
-        this.movie = movie;
-        this.age = age;
+        this.information = information;
     }
 
     public Long getId() {
@@ -30,35 +25,11 @@ public class Avenger {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getInformation() {
+        return information;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSuperheroName() {
-        return superheroName;
-    }
-
-    public void setSuperheroName(String superheroName) {
-        this.superheroName = superheroName;
-    }
-
-    public String getMovie() {
-        return movie;
-    }
-
-    public void setMovie(String movie) {
-        this.movie = movie;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
+    public void setInformation(String information) {
+        this.information = information;
     }
 }
