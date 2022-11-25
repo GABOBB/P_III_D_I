1062cf26afebb061180a5e915936bbb623c8abf7
arty4325 <arturo.acuna.duran@gmail.com>
2022-11-25 00:03:16 -0600

    Web socket implementacion

---
+++ ReactApplication/frontend/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Appbar from './components/Appbar';
+import MatrixVector from "./components/MatrixVector";
+
+function App() {
+  return (
+    <div className="App">
+    <Appbar/>
+    <MatrixVector/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactApplication/frontend/src/components/MatrixVector.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+
+export default function MatrixVector() {
+  return (
+    <Stack direction="row" alignItems="center" spacing={2}>
+      <Button variant="contained" component="label">
+        Boton
+      </Button>
+    </Stack>
+  );
+}
